66e4651d84dff216367665fa028324bbb0644f39
Patrice Dargenton <patrice.dargenton@free.fr>
2023-11-22 11:46:36 +0100

    First commit !

---
+++ BlazorAppWA/Pages/Counter2.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorAppWA.Pages
+{
+    public class Counter2Base: ComponentBase
+    {
+        protected int currentCount = 0;
+        
+        [Parameter]
+        public int Value { get => currentCount; set => currentCount = value; }
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
---
+++ BlazorAppWA/Pages/NombreMagique.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorAppWA.Pages
+{
+	// D'après :
+	// [TUTORIAL POUR DÉBUTANT] Découverte de BLAZOR - Créer des applications web DE FOLIE
+	// https://www.youtube.com/watch?v=bPJOASSGKrQ
+
+	public class NombreMagiqueBase : ComponentBase
+    {
+        protected const int NbLifesMax = 5;
+		protected const int NbMax = 10;
+
+		protected int value;
+
+		protected int nbMagic;
+		protected int nbRemainingLifes;
+		protected bool? gameWon;
+
+		protected override void OnInitialized()
+		{
+			ReinitGame();
+			base.OnInitialized();
+		}
+
+		protected void ReinitGame()
+		{
+			var random = new Random();
+			nbMagic = random.Next(NbMax);
+			nbRemainingLifes = NbLifesMax;
+			gameWon = null;
+			value = 0;
+		}
+
+		protected void TestNumber() 
+		{ 
+			if (value == nbMagic)
+			{
+				gameWon = true;
+			}
+            else
+            {
+				nbRemainingLifes--;
+				if (nbRemainingLifes == 0)
+				{
+					gameWon = false;
+				}
+            }
+        }
+	}
+}
